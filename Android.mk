f5f7b0121c483badacaff01bd4f16e826b99cb5c
icecat <icecatm7@gmail.com>
2026-08-12 09:02:41 +0700

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,9 +1,4 @@
-#
-# Copyright (C) 2022 The Android Open Source Project
-# Copyright (C) 2022 The TWRP Open Source Project
-#
-# SPDX-License-Identifier: Apache-2.0
-#
+
 
 LOCAL_PATH := $(call my-dir)
 
