36badd37216a0f43c0bb96091f5ea3f997783530
icecat <icecatm7@gmail.com>
2026-08-12 19:51:43 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,29 +1,28 @@
-#
-# Copyright (C) 2022 The Android Open Source Project
-# Copyright (C) 2022 The TWRP Open Source Project
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-# For building with minimal manifest
+# Core Build Environment
 export ALLOW_MISSING_DEPENDENCIES="true"
+export LC_ALL="C"
+# Screen/Status Bar Adjustments
 export OF_SCREEN_H="2400"
 export OF_STATUS_H="140"
 export OF_STATUS_INDENT_LEFT="48"
 export OF_STATUS_INDENT_RIGHT="48"
+# Security and Feature Overrides
 export OF_USE_MAGISKBOOT="1"
 export OF_NO_TREBLE_COMPATIBILITY_CHECK="1"
 export OF_SKIP_MULTIUSER_FOLDERS_BACKUP="1"
-export LC_ALL="C"
+# Utilities and Build Configuration
 export FOX_USE_NANO_EDITOR="1"
 export FOX_DELETE_AROMAFM="1"
 export FOX_USE_TAR_BINARY="1"
-export FOX_DYNAMIC_SAMSUNG_FIX="1"
 export FOX_USE_SED_BINARY="1"
 export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER="1"
 export FOX_USE_XZ_UTILS="1"
+# Device-Specific Fixes
+export FOX_DYNAMIC_SAMSUNG_FIX="1"
+# Information
 export OF_MAINTAINER="Icecat"
 export FOX_BUILD_TYPE="Stable"
 export FOX_MAINTAINER_PATCH_VERSION="1"
-export OF_FL_PATH1="/tmp/flashlight" # See /init.recovery.qcom.rc for more information
+# Hardware
+export OF_FL_PATH1="/tmp/flashlight"
 export OF_FL_PATH2=""
